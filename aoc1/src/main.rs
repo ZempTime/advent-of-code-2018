33d003df464f5bb5786a3e93f82fc5666275807f
Chris Zempel <chris.m.zempel@gmail.com>
2019-08-17 10:21:02 -0500

    Add aoc1 part 2

--- aoc1/src/main.rs
+++ aoc1/src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{self, Read, Write};
 
+use std::collections::HashSet;
+use std::io::{self, Read, Write};
 // This is called "boxing an error" -> https://doc.rust-lang.org/rust-by-example/error/multiple_error_types/boxing_errors.html
 // basically it puts all your errors in a box, which is a way of storing a reference to a thing where you don't know how big it will be
 type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
@@ -8,11 +9,9 @@ type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
 fn main() -> Result<()> {
     let mut input = String::new();
 
-    // io::stdin() waits for user input. invoke this by `cargo run < input/input.txt`
-    // this ? operator can only be used on functions which return a type of Result
-    // See Yehuda Katz' comments in the discussion here, really interesting: https://internals.rust-lang.org/t/the-operator-will-be-harmful-to-rust/3882/5
     io::stdin().read_to_string(&mut input)?;
     part1(&input)?;
+    part2(&input)?;
 
     // () is a value of the type () and its purpose is to be useless.
     // see https://stackoverflow.com/questions/24842271/what-is-the-purpose-of-the-unit-type-in-rust for more deets
@@ -29,4 +28,24 @@ fn part1(input: &str) -> Result<()> {
     // uses "Write" imported at top
     writeln!(io::stdout(), "{}", freq)?;
     Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    let mut freq = 0;
+    // see https://doc.rust-lang.org/std/collections/struct.HashSet.html
+    let mut record = HashSet::new();
+    record.insert(0);
+
+    // Why do I need this loop?
+    loop {
+        for line in input.lines() {
+            let change: i32 = line.parse().unwrap();
+            freq += change;
+            if record.contains(&freq) {
+                writeln!(io::stdout(), "{}", freq)?;
+                return Ok(());
+            }
+            record.insert(freq);
+        }
+    }
 }
